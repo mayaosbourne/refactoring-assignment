ccafa64019c25433e4e4d97579765883bdcf19e9
mayaosbourne <mayajosbourne@gmail.com>
2014-04-24 22:41:26 -0700

    customer class

---
+++ TCSS490_Refactor/src/Customer.java
@@ -0,0 +1,17 @@
+
+public class Customer {
+	public String fName;
+	public String fStreet;
+	public String fZipcode;
+	public String fTown;
+	public String fCountry;
+
+	public Customer(String name, String street, String zipCode, String town, String country) {
+		fName = name;
+		fStreet = street;
+		fZipcode = zipCode;
+		fTown = town;
+		fCountry = country;
+	}
+	
+}
