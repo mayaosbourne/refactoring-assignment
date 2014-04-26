39243019f026d939d07f528673f9a3ebe92bfe7c
Dawnelle Rocks <drocks@uw.edu>
2014-04-25 22:39:14 -0700

    Added tests for creditcard, customer and debitcard classes.

---
+++ TCSS490_Refactor/src/DebitCardTest2.java
@@ -0,0 +1,48 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author Dawn
+ *
+ */
+public class DebitCardTest2
+{
+
+	private Customer customerOne;
+	private CheckingAccount checkingAccOne;
+	private CheckingAccount checkingAccTwo;
+	private DebitCard debitCardOne;
+	private DebitCard debitCardTwo;
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception
+	{
+		customerOne = new Customer("Dawn Rocks", "Mocking Bird Ln", 
+				"98146", "Seattle", "USA");
+		checkingAccOne = new CheckingAccount(customerOne);
+		debitCardOne = new DebitCard(0, checkingAccOne);
+		checkingAccTwo = new CheckingAccount(customerOne, 1);
+		debitCardTwo = new DebitCard(10, checkingAccTwo);
+		checkingAccTwo.increaseBalance(20.00);
+	}
+
+	/**
+	 * Test method for {@link DebitCard#pay(double)}.
+	 */
+	@Test
+	public void testPay()
+	{
+		assertTrue("Testing if", debitCardOne.pay(30.00));
+		assertFalse("Testing else", debitCardTwo.pay(20.00));
+	}
+
+}
